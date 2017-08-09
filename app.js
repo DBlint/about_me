8e0a796a915672a54c72363472771cf3ab7fe780
Rob Bronson <robert.j.bronson@gmail.com>
2017-08-08 17:13:40 -0700

    Fixed vague variable names, and added console logs

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 'strict';
 
-var q1 = 'Am I married?';
-var q2 = 'Do I have kids?';
-var q3 = 'Did I play sports in school?';
-var q4 = 'Are my kids out of the house?';
-var q5 = 'Have I ever been out of the country?';
+var question1 = 'Am I married?';
+var question2 = 'Do I have kids?';
+var question3 = 'Did I play sports in school?';
+var question4 = 'Are my kids out of the house?';
+var question5 = 'Have I ever been out of the country?';
 
-var a1 = prompt(q1).toUpperCase();
-var a2 = prompt(q2).toUpperCase();
-var a3 = prompt(q3).toUpperCase();
-var a4 = prompt(q4).toUpperCase();
-var a5 = prompt(q5).toUpperCase();
+var answer1 = prompt(question1).toUpperCase();
+console.log('Answer1 = ' + answer1);
+var answer2 = prompt(question2).toUpperCase();
+console.log('Answer2 = ' + answer2);
+var answer3 = prompt(question3).toUpperCase();
+console.log('Answer3 = ' + answer3);
+var answer4 = prompt(question4).toUpperCase();
+console.log('Answer4 = ' + answer4);
+var answer5 = prompt(question5).toUpperCase();
+console.log('Answer5 = ' + answer5);
 
 var text1, text2, text3, text4, text5;
 
-if (a1 === 'YES' || a1 === 'Y')
+if (answer1 === 'YES' || answer1 === 'Y')
   {
   text1 = document.getElementById('answer1');
   text1.textContent = 'You are right! I have been married to my wife for 21 years.';
 }
-else if (a1 === 'NO' || a1 === 'N'){
+else if (answer1 === 'NO' || answer1 === 'N'){
   text1 = document.getElementById('answer1');
   text1.textContent = 'NOPE! I have actually been married to my wife for 21 years.';
 }
@@ -29,12 +34,12 @@ else {
   text1.textContent = 'You have not entered your answer in the proper format.';
 }
 
-if (a2 === 'YES' || a2 === 'Y')
+if (answer2 === 'YES' || answer2 === 'Y')
   {
   text2 = document.getElementById('answer2');
   text2.textContent = 'You are correct!  I have a son and a daughter.';
 }
-else if (a2 === 'NO' || a2 === 'N'){
+else if (answer2 === 'NO' || answer2 === 'N'){
   text2 = document.getElementById('answer2');
   text2.textContent = 'NOPE! I have have 2 children.  A son and a daughter.';
 }
@@ -44,12 +49,12 @@ else {
   text2.textContent = 'You have not entered your answer in the proper format.';
 }
 
-if (a3 === 'YES' || a3 === 'Y')
+if (answer3 === 'YES' || answer3 === 'Y')
   {
   text3 = document.getElementById('answer3');
   text3.textContent = 'Sorry to disappoint.  I did not participate in school sports.';
 }
-else if (a3 === 'NO' || a3 === 'N'){
+else if (answer3 === 'NO' || answer3 === 'N'){
   text3 = document.getElementById('answer3');
   text3.textContent = 'Affirmative!  I did not participate in school sports.';
 }
@@ -59,12 +64,12 @@ else {
   text3.textContent = 'You have not entered your answer in the proper format.';
 }
 
-if (a4 === 'YES' || a4 === 'Y')
+if (answer4 === 'YES' || answer4 === 'Y')
   {
   text4 = document.getElementById('answer4');
   text4.textContent = 'Hell yeah!  My daughter is 19 and currently attending WSU!';
 }
-else if (a4 === 'NO' || a4 === 'N'){
+else if (answer4 === 'NO' || answer4 === 'N'){
   text4 = document.getElementById('answer4');
   text4.textContent = 'Actually, my 19 year old daughter is at WSU now!';
 }
@@ -74,12 +79,12 @@ else {
   text4.textContent = 'You have not entered your answer in the proper format.';
 }
 
-if (a5 === 'YES' || a5 === 'Y')
+if (answer5 === 'YES' || answer5 === 'Y')
   {
   text5 = document.getElementById('answer5');
   text5.textContent = 'Yes I have.  I have set foot in Mexico, Spain, Afghanistan, Iraq, Kyrgyzstan, Ireland, Kuwait, and Canada.';
 }
-else if (a1 === 'NO' || a1 === 'N'){
+else if (answer5 === 'NO' || answer5 === 'N'){
   text5 = document.getElementById('answer5');
   text5.textContent = 'Actually, my time in the Army allowed me to visit several countries.';
 }
