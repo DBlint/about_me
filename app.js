17757da7e1ae9370117a35c7fdda1cb12530e385
Rob Bronson <robert.j.bronson@gmail.com>
2017-08-09 15:47:30 -0700

    Adding get user name input and adding to Alert messages.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'strict';
+'use strict';
 
 var question1 = 'Am I married?';
 var question2 = 'Do I have kids?';
@@ -6,6 +6,7 @@ var question3 = 'Did I play sports in school?';
 var question4 = 'Are my kids out of the house?';
 var question5 = 'Have I ever been out of the country?';
 
+var guestName = prompt('Please enter your name!');
 var answer1 = prompt(question1).toUpperCase();
 console.log('Answer1 = ' + answer1);
 var answer2 = prompt(question2).toUpperCase();
@@ -21,12 +22,12 @@ var text1, text2, text3, text4, text5;
 
 if (answer1 === 'YES' || answer1 === 'Y')
   {
-  alert('You are right! I have been married to my wife for 21 years.');
+  alert('You are right! ' + guestName + ' I have been married to my wife for 21 years.');
   text1 = document.getElementById('answer1');
   text1.textContent = 'You are right! I have been married to my wife for 21 years.';
 }
 else if (answer1 === 'NO' || answer1 === 'N'){
-  alert('NOPE! I have actually been married to my wife for 21 years.');
+  alert('NO WAY ' + guestName.toUpperCase() + '! I have actually been married to my wife for 21 years.');
   text1 = document.getElementById('answer1');
   text1.textContent = 'NOPE! I have actually been married to my wife for 21 years.';
 }
@@ -38,12 +39,12 @@ else {
 
 if (answer2 === 'YES' || answer2 === 'Y')
   {
-  alert('You are correct!  I have a son and a daughter.');
+  alert('You are correct ' + guestName + '!  I have a son and a daughter.');
   text2 = document.getElementById('answer2');
-  text2.textContent = 'You are correct!  I have a son and a daughter.';
+  text2.textContent = 'You are correct ' + guestName + '! I have a son and a daughter.';
 }
 else if (answer2 === 'NO' || answer2 === 'N'){
-  alert('NOPE! I have have 2 children.  A son and a daughter.');
+  alert('NO WAY ' + guestName.toUpperCase() + '! I have have 2 children.  A son and a daughter.');
   text2 = document.getElementById('answer2');
   text2.textContent = 'NOPE! I have have 2 children.  A son and a daughter.';
 }
@@ -55,12 +56,12 @@ else {
 
 if (answer3 === 'YES' || answer3 === 'Y')
   {
-  alert('Sorry to disappoint.  I did not participate in school sports.');
+  alert('Sorry to disappoint you . ' + guestName + ' I did not participate in school sports.');
   text3 = document.getElementById('answer3');
   text3.textContent = 'Sorry to disappoint.  I did not participate in school sports.';
 }
 else if (answer3 === 'NO' || answer3 === 'N'){
-  alert('Affirmative!  I did not participate in school sports.');
+  alert('Affirmative ' + guestName + '! I did not participate in school sports.');
   text3 = document.getElementById('answer3');
   text3.textContent = 'Affirmative!  I did not participate in school sports.';
 }
@@ -72,12 +73,12 @@ else {
 
 if (answer4 === 'YES' || answer4 === 'Y')
   {
-  alert('Hell yeah!  My daughter is 19 and currently attending WSU!');
+  alert('Hell yeah ' + guestName + '! My daughter is 19 and currently attending WSU!');
   text4 = document.getElementById('answer4');
   text4.textContent = 'Hell yeah!  My daughter is 19 and currently attending WSU!';
 }
 else if (answer4 === 'NO' || answer4 === 'N'){
-  alert('Actually, my 19 year old daughter is at WSU now!');
+  alert('Actually ' + guestName + ', my 19 year old daughter is at WSU now!');
   text4 = document.getElementById('answer4');
   text4.textContent = 'Actually, my 19 year old daughter is at WSU now!';
 }
@@ -89,12 +90,12 @@ else {
 
 if (answer5 === 'YES' || answer5 === 'Y')
   {
-  alert('Yes I have.  I have set foot in Mexico, Spain, Afghanistan, Iraq, Kyrgyzstan, Ireland, Kuwait, and Canada.');
+  alert('Yes  ' + guestName + ', I have.  I have set foot in Mexico, Spain, Afghanistan, Iraq, Kyrgyzstan, Ireland, Kuwait, and Canada.');
   text5 = document.getElementById('answer5');
   text5.textContent = 'Yes I have.  I have set foot in Mexico, Spain, Afghanistan, Iraq, Kyrgyzstan, Ireland, Kuwait, and Canada.';
 }
 else if (answer5 === 'NO' || answer5 === 'N'){
-  alert('Actually, my time in the Army allowed me to visit several countries.');
+  alert('Actually ' + guestName + ', my time in the Army allowed me to visit several countries.');
   text5 = document.getElementById('answer5');
   text5.textContent = 'Actually, my time in the Army allowed me to visit several countries.';
 }
